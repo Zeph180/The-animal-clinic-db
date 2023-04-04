cf3e123f72e03198df34d08bfaf78423f26744d9
zeph180 <zephrichards1@gmail.com>
2023-04-04 19:30:39 +0300

    All queries

--- queries.sql
+++ queries.sql
@@ -2,3 +2,17 @@
 
 -- select name ending with mon
 SELECT * FROM animals WHERE NAME like '%mon'
+
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+SELECT name from animals WHERE neutered = true AND escape_attempts < 3;
+
+SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5; 
+
+SELECT * from animals WHERE neutered = true;
+
+SELECT * from animals WHERE name != 'Gabumon';
+
+SELECT * from animals WHERE weight_kg BETWEEN 10.4 and 17.3;
--- schema.sql
+++ schema.sql
@@ -1,16 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
 -- creating database
--- CREATE DATABASE vet_clinic;
+CREATE DATABASE vet_clinic;
 
 -- -- create table animals
--- CREATE TABLE ANIMALS(
---     ID INTEGER NOT NULL,
---     NAME VARCHAR(20),
---     DATE_OF_BIRTH DATE,
---     ESCAPE_ATTEMPTS INTEGER,
---     NEUTERED BOOLEAN,
---     WEIGHT_KG DECIMAL
--- );
-
-SELECT * FROM animals;
+CREATE TABLE ANIMALS(
+    ID INTEGER NOT NULL,
+    NAME VARCHAR(20),
+    DATE_OF_BIRTH DATE,
+    ESCAPE_ATTEMPTS INTEGER,
+    NEUTERED BOOLEAN,
+    WEIGHT_KG DECIMAL
+);
